f57aeb4e3fc7ee9a221da0af640c0669b2e2de58
omotega <tomoyibo@gmail.com>
2023-09-07 09:41:17 +0100

    get request endpoint

--- index.js
+++ index.js
@@ -8,15 +8,24 @@ const app = express();
 app.get("/", (req, res) => {
   const { slack_name, track } = req.query;
   const dateValue = new Date();
+  const utcTime = new Date(
+    currentDate.getTime() + currentDate.getTimezoneOffset() * 60000
+  );
+  const utcOffset2 = new Date(
+    utcTime.getTime() + 2 * 60 * 60 * 1000
+  ).toISOString();
+
   const data = {
     slack_name: slack_name,
     current_day: dateValue.getDay(),
-    utc_time: "",
+    utc_time: utcOffset2,
     track: track,
-    github_file_url: "",
-    github_repo: "",
+    github_file_url:
+      "https://github.com/omotega/hng-taskone/blob/main/index.js",
+    github_repo: "https://github.com/omotega/hng-taskone",
     statuscode: 200,
   };
+  console.log(data);
   res.status(200).json(data);
 });
 

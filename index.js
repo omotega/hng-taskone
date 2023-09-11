372848a3e50274add85cb8eaaeb9e65484b8ca18
omotega <tomoyibo@gmail.com>
2023-09-11 19:05:44 +0100

    feat: crud endpoits for user

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { getCurrentUTCTime } = require("./date");
-const personRoute = require('./src/routes/person')
+const personRoute = require("./src/routes/person");
 dotenv.config();
 
 const app = express();
@@ -13,35 +13,35 @@ app.get("/", (req, res) => {
   res.send("welcome to my hng task");
 });
 
-// app.get("/api", (req, res) => {
-//   const { slack_name, track } = req.query;
-//   const dateValue = new Date();
+app.get("/api", (req, res) => {
+  const { slack_name, track } = req.query;
+  const dateValue = new Date();
 
-//   const daysOfWeek = [
-//     "Sunday",
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday",
-//   ];
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-//   const dayName = daysOfWeek[dateValue.getDay()];
-//   const utcdate = getCurrentUTCTime();
+  const dayName = daysOfWeek[dateValue.getDay()];
+  const utcdate = getCurrentUTCTime();
 
-//   const data = {
-//     slack_name: slack_name,
-//     current_day: dayName,
-//     utc_time: utcdate,
-//     track: track,
-//     github_file_url:
-//       "https://github.com/omotega/hng-taskone/blob/main/index.js",
-//     github_repo_url: "https://github.com/omotega/hng-taskone",
-//     status_code: 200,
-//   };
-//   res.status(200).json(data);
-// });
+  const data = {
+    slack_name: slack_name,
+    current_day: dayName,
+    utc_time: utcdate,
+    track: track,
+    github_file_url:
+      "https://github.com/omotega/hng-taskone/blob/main/index.js",
+    github_repo_url: "https://github.com/omotega/hng-taskone",
+    status_code: 200,
+  };
+  res.status(200).json(data);
+});
 
 app.use("/api", personRoute);
 
